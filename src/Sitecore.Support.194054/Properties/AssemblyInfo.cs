78699a4ee2df06aa048b02c4a74668007c7303b5
sc-AlexanderNg <alex@sitecore.net>
2017-11-17 16:34:46 +0800

    Fixed itemBuckets.js to call !SC.searchBoxViewModel.isSearchEmpty() && SC.searchBoxViewModel.startSearchCallback() after EstablishViews()

---
+++ src/Sitecore.Support.194054/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.194054")]
+[assembly: AssemblyProduct("Sitecore.Support.194054")]
+[assembly: ComVisible(false)]
